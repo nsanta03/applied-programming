cdc6b24a82f4b143eb329e84aae4d0da82f8b1a8
nsanta03 <nick.santanjelo@gmail.com>
2020-05-17 00:24:51 +0000

    commit

--- dice.rb
+++ dice.rb
@@ -3,8 +3,11 @@ require "sinatra/reloader"
 def view(template); erb template.to_sym; end
 
 get "/" do
-    @die1=rand(1..6)
-    @die2=rand(1..6)
+    @dice=[]
+    number_of_dice=9
+    number_of_dice.times do
+        @dice << rand(1..6)
+    end
     view "dice"
 end
 
