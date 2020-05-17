561ad34207275470eb7e46d0490067b3bca64e68
nsanta03 <nick.santanjelo@gmail.com>
2020-05-17 16:22:30 +0000

    commit

--- cards.rb
+++ cards.rb
@@ -2,21 +2,18 @@ require "sinatra"
 require "sinatra/reloader"
 def view(template); erb template.to_sym; end
 
-ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king", "ace"]
-suits = ["clubs", "diamonds", "hearts", "spades"]
-
-deck=[]
-for rank in ranks 
-    for suit in suits
-        deck << "#{rank}_of_#{suit}"
-    end
-end    
 
 get "/" do
-  @hand=[]
-  number_of_cards=5
-  number_of_cards.times do
-    @hand<<puts deck.shuffle 
-  end  
-  view 'cards'
+    ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king", "ace"]
+    suits = ["clubs", "diamonds", "hearts", "spades"]
+    deck=[]
+
+    for rank in ranks 
+        for suit in suits
+            deck << "#{rank}_of_#{suit}"
+        end
+    end   
+   shuffled_deck=deck.shuffle
+   @hand=shuffled_deck[0,5]
+   view 'cards'
 end
