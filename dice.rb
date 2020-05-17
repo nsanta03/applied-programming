372dae1d5b3c705cd9de719c35ed9d0df81d196c
nsanta03 <nick.santanjelo@gmail.com>
2020-05-17 00:10:17 +0000

    commit

--- dice.rb
+++ dice.rb
@@ -3,5 +3,11 @@ require "sinatra/reloader"
 def view(template); erb template.to_sym; end
 
 get "/" do
-  "<h1>""tacos""</h1>"
+    @die1=rand(1..6)
+    @die2=rand(1..6)
+    view "dice"
+end
+
+get "/tacos" do 
+    "<h1>Tacos</h1>"
 end
